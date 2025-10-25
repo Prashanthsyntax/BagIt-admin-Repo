0c91cb4ee4e2335012d07068e3fd1298eec37e62
Prashanthsyntax <pendemprashanth49@gmail.com>
2025-10-26 00:01:21 +0530

    route setup1

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -11,7 +11,6 @@ export default function RootLayout({
 
 
     return <>
-    <p>Admin layout</p>
         {children}
     </>
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1>Root Layout</h1>
         <main>
           {children}
         </main>
