40cbd7bc382020019d5a70ca999bf162a3aa3eb5
Prashanthsyntax <pendemprashanth49@gmail.com>
2025-10-26 17:06:09 +0530

    code layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -12,26 +12,29 @@ export default async function AdminLayout({
   children: ReactNode;
 }>) {
   const supabase = await createClient();
-  
-      const { data: authData } = await supabase.auth.getUser();
-  
-      if(authData?.user) {
-          const { data, error } = await supabase.from('users')
-          .select("*")
-          .eq('id', authData.user.id)
-          .single();
-  
-          if(error || !data) {
-              console.log('Error fetching user data', error);
-              return;
-          }
-  
-          if(data.type === ADMIN) return redirect('/');
-      }
 
-  return <RenderMounted>
-    <Header />
-    <main className="min-h-[calc(100svh-128px)] py-3">{children}</main>
-    <Footer />
-    </RenderMounted>;
+  const { data: authData } = await supabase.auth.getUser();
+
+  if (authData?.user) {
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", authData.user.id)
+      .single();
+
+    if (error || !data) {
+      console.log("Error fetching user data", error);
+      return;
+    }
+
+    if (data.type === ADMIN) return redirect("/");
+  }
+
+  return (
+    <RenderMounted>
+      <Header />
+      <main className="min-h-[calc(100svh-128px)] py-3">{children}</main>
+      <Footer />
+    </RenderMounted>
+  );
 }
