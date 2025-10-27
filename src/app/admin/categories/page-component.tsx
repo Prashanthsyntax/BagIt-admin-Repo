51f631f9d10b668c36e71079764b7ceef1b7251a
Prashanthsyntax <pendemprashanth49@gmail.com>
2025-10-27 23:16:09 +0530

    category ui started

--- src/app/admin/categories/page-component.tsx
+++ src/app/admin/categories/page-component.tsx
@@ -2,7 +2,7 @@
 
 import { FC, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { PlusCircle } from "lucide-react";
+import { Plus, PlusCircle } from "lucide-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/ui/button";
@@ -64,8 +64,37 @@ const CategoriesPageComponent: FC<Props> = ({ categories }) => {
 
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-      <div>
-        
+      <div className="flex items-center my-10">
+        <div className="ml-auto flex items-center gap-2">
+          <Dialog
+            open={isCreateCategoryModalOpen}
+            onOpenChange={() =>
+              setIsCreateCategoryModalOpen(!isCreateCategoryModalOpen)
+            }
+          >
+            <DialogTrigger asChild>
+              <Button
+                size="sm"
+                className="h-8 gap-1"
+                onClick={() => {
+                  setCurrentCategory(null);
+                  setIsCreateCategoryModalOpen(true);
+                }}
+              >
+                <PlusCircle className="h-3.5 w-3.5" />
+                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                  Add Category
+                </span>
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create Category</DialogTitle>
+                </DialogHeader>
+                <CategoryForm form={form} onSubmit={submitCategoryHandler} defaultValues={currentCategory} />
+            </DialogContent>
+          </Dialog>
+        </div>
       </div>
     </main>
   );
