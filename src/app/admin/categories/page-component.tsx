1e6c51d53ad693fa57f2f36f379d401740f30830
Prashanthsyntax <pendemprashanth49@gmail.com>
2025-10-27 23:00:02 +0530

    category ui implementation

--- src/app/admin/categories/page-component.tsx
+++ src/app/admin/categories/page-component.tsx
@@ -1,50 +1,74 @@
-'use client'
+"use client";
 
-import { FC, useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { PlusCircle } from 'lucide-react';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { FC, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { PlusCircle } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
+} from "@/components/ui/card";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog';
+} from "@/components/ui/dialog";
 import {
   Table,
   TableBody,
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table';
+} from "@/components/ui/table";
 
-import { CategoryTableRow } from '@/components/category';
+import { CategoryTableRow } from "@/components/category";
 import {
   createCategorySchema,
   CreateCategorySchema,
-} from '@/app/admin/categories/create-category.schema';
-import { CategoriesWithProductsResponse } from '@/app/admin/categories/categories.types';
-import { CategoryForm } from '@/app/admin/categories/category-form';
-
+} from "@/app/admin/categories/create-category.schema";
+import { CategoriesWithProductsResponse } from "@/app/admin/categories/categories.types";
+import { CategoryForm } from "@/app/admin/categories/category-form";
+import { date } from "zod";
+import { da } from "zod/v4/locales";
 
 type Props = {
-    categories: CategoriesWithProductsResponse;
+  categories: CategoriesWithProductsResponse;
 };
 
 const CategoriesPageComponent: FC<Props> = ({ categories }) => {
+  const [isCreateCategoryModalOpen, setIsCreateCategoryModalOpen] =
+    useState(false);
+  const [currentCategory, setCurrentCategory] =
+    useState<CreateCategorySchema | null>(null);
 
-    return <>CategoriesPageComponent</>
+  const form = useForm<CreateCategorySchema>({
+    resolver: zodResolver(createCategorySchema),
+    defaultValues: {
+      name: "",
+      image: undefined,
+    },
+  });
+
+  const submitCategoryHandler: SubmitHandler<CreateCategorySchema> = async (
+    data
+  ) => {
+    console.log(data);
+  };
+
+  return (
+    <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+      <div>
+        
+      </div>
+    </main>
+  );
 };
-
 
 export default CategoriesPageComponent;
