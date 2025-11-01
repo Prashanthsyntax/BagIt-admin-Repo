65a61b9779b479f2cb9b895dc0219b9e0c38226a
Prashanthsyntax <pendemprashanth49@gmail.com>
2025-11-01 15:57:44 +0530

    building the page component

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -1,17 +1,19 @@
-import { getOrdersWithProducts } from "@/actions/orders";
-import React from "react";
+import { getOrdersWithProducts } from '@/actions/orders';
+import PageComponent from '@/app/admin/orders/page-component';
 
 const Orders = async () => {
   const ordersWithProducts = await getOrdersWithProducts();
 
   if (!ordersWithProducts)
     return (
-      <div className="text-center font-bold text-2xl">No orders found</div>
+      <div className='text-center font-bold text-2xl'>No orders found</div>
     );
 
-//   console.log(ordersWithProducts);
-
-  return <div>Orders Page</div>;
+  return (
+    <div>
+      <PageComponent ordersWithProducts={ordersWithProducts} />
+    </div>
+  );
 };
 
 export default Orders;
